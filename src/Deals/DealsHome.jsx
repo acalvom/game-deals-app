bc0d26b8ddeb58bdffa410437ba4839649de947c
acalvom <andreac.3108@gmail.com>
2021-11-26 15:00:16 +0100

    Add mock deals & Add responsive styles

--- src/Deals/DealsHome.jsx
+++ src/Deals/DealsHome.jsx
@@ -1,7 +1,63 @@
+import './Deals.css';
+
 function DealsHome() {
     return (
-        <div className="container">
+        <div className="container-fluid">
             <h2 className="text-secondary"> Deals Home Page</h2>
+            <div className="list-deals list-group">
+                <a href="#" className="list-group-item list-group-item-action">
+                    <div className="img-deal d-none d-sm-block">
+                        <img src="https://via.placeholder.com/120" className="img-fluid" alt=""/>
+                    </div>
+                    <div className="text-deal">
+                        <div className="d-flex w-100 justify-content-between">
+                            <h5 className="mb-1">List group item heading</h5>
+                            <small>3 days ago</small>
+                        </div>
+                        <p className="mb-1">Some placeholder content in a paragraph.</p>
+                        <small>And some small print.</small>
+                    </div>
+                </a>
+                <a href="#" className="list-group-item list-group-item-action">
+                    <div className="img-deal d-none d-sm-block ">
+                        <img src="https://via.placeholder.com/120" className="rounded float-start" alt=""/>
+                    </div>
+                    <div className="text-deal">
+                        <div className="d-flex w-100 justify-content-between">
+                            <h5 className="mb-1">List group item heading</h5>
+                            <small>3 days ago</small>
+                        </div>
+                        <p className="mb-1">Some placeholder content in a paragraph.</p>
+                        <small>And some small print.</small>
+                    </div>
+                </a>
+                <a href="#" className="list-group-item list-group-item-action">
+                    <div className="img-deal d-none d-sm-block">
+                        <img src="https://via.placeholder.com/120" className="img-fluid" alt=""/>
+                    </div>
+                    <div className="text-deal">
+                        <div className="d-flex w-100 justify-content-between">
+                            <h5 className="mb-1">List group item heading</h5>
+                            <small>3 days ago</small>
+                        </div>
+                        <p className="mb-1">Some placeholder content in a paragraph.</p>
+                        <small>And some small print.</small>
+                    </div>
+                </a>
+                <a href="#" className="list-group-item list-group-item-action">
+                    <div className="img-deal d-none d-sm-block ">
+                        <img src="https://via.placeholder.com/120" className="rounded float-start" alt=""/>
+                    </div>
+                    <div className="text-deal">
+                        <div className="d-flex w-100 justify-content-between">
+                            <h5 className="mb-1">List group item heading</h5>
+                            <small>3 days ago</small>
+                        </div>
+                        <p className="mb-1">Some placeholder content in a paragraph.</p>
+                        <small>And some small print.</small>
+                    </div>
+                </a>
+            </div>
         </div>
     );
 }
