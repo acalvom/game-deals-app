a6ed411ac04dfc8d9ea2f19cfbfd7541a6f343b0
acalvom <andreac.3108@gmail.com>
2021-11-26 12:27:33 +0100

    Add Deals Home Page skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import DealsHome from "./Deals/DealsHome";
+
 const App = () => {
     return (
-        <div className="container">
-            <h1 className="text-primary">Game Deals App</h1>
+        <div className="container mt-5">
+            <DealsHome/>
         </div>
     );
 };
---
+++ src/Deals/DealsHome.jsx
@@ -0,0 +1,9 @@
+function DealsHome() {
+    return (
+        <div className="container">
+            <h2 className="text-secondary"> Deals Home Page</h2>
+        </div>
+    );
+}
+
+export default DealsHome;
