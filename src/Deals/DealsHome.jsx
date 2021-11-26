19f25d06105131023f203ef9a312d00b11a730e1
acalvom <andreac.3108@gmail.com>
2021-11-26 15:45:05 +0100

    Prepare mock deals & Adapt styles

--- src/Deals/DealsHome.jsx
+++ src/Deals/DealsHome.jsx
@@ -1,6 +1,13 @@
 import './Deals.css';
 
 function DealsHome() {
+
+    const deals = [
+        {title:'' , salePrice: 55, normalPrice: 22, savings: 66, steamRatingText:' ',dealRating:'', releaseDate:333333, lastChange:44444,thumb: 'https://via.placeholder.com/48'},
+        {title:'' , salePrice: 55, normalPrice: 22, savings: 66, steamRatingText:' ',dealRating:'', releaseDate:333333, lastChange:44444,thumb: 'https://via.placeholder.com/48'},
+        {title:'' , salePrice: 55, normalPrice: 22, savings: 66, steamRatingText:' ',dealRating:'', releaseDate:333333, lastChange:44444,thumb: 'https://via.placeholder.com/48'}
+    ]
+
     return (
         <div className="container-fluid">
             <h2 className="text-secondary"> Deals Home Page</h2>
@@ -11,24 +18,17 @@ function DealsHome() {
                     </div>
                     <div className="text-deal">
                         <div className="d-flex w-100 justify-content-between">
-                            <h5 className="mb-1">List group item heading</h5>
+                            <h5 className="mb-1">Game Title</h5>
                             <small>3 days ago</small>
                         </div>
-                        <p className="mb-1">Some placeholder content in a paragraph.</p>
-                        <small>And some small print.</small>
-                    </div>
-                </a>
-                <a href="#" className="list-group-item list-group-item-action">
-                    <div className="img-deal d-none d-sm-block ">
-                        <img src="https://via.placeholder.com/120" className="rounded float-start" alt=""/>
-                    </div>
-                    <div className="text-deal">
-                        <div className="d-flex w-100 justify-content-between">
-                            <h5 className="mb-1">List group item heading</h5>
-                            <small>3 days ago</small>
+                        <div className="d-flex w-100 justify-content-end">
+                            <span>Sale Price</span>
+                            <span>Normal Price</span>
+                            <span>Discount</span>
                         </div>
-                        <p className="mb-1">Some placeholder content in a paragraph.</p>
-                        <small>And some small print.</small>
+
+                        <p className="mb-1">Deal rating - Deal text</p>
+                        <small>Release sale date</small>
                     </div>
                 </a>
                 <a href="#" className="list-group-item list-group-item-action">
@@ -37,24 +37,17 @@ function DealsHome() {
                     </div>
                     <div className="text-deal">
                         <div className="d-flex w-100 justify-content-between">
-                            <h5 className="mb-1">List group item heading</h5>
+                            <h5 className="mb-1">Game Title</h5>
                             <small>3 days ago</small>
                         </div>
-                        <p className="mb-1">Some placeholder content in a paragraph.</p>
-                        <small>And some small print.</small>
-                    </div>
-                </a>
-                <a href="#" className="list-group-item list-group-item-action">
-                    <div className="img-deal d-none d-sm-block ">
-                        <img src="https://via.placeholder.com/120" className="rounded float-start" alt=""/>
-                    </div>
-                    <div className="text-deal">
-                        <div className="d-flex w-100 justify-content-between">
-                            <h5 className="mb-1">List group item heading</h5>
-                            <small>3 days ago</small>
+                        <div className="d-flex w-100 justify-content-end">
+                            <span>Sale Price</span>
+                            <span>Normal Price</span>
+                            <span>Discount</span>
                         </div>
-                        <p className="mb-1">Some placeholder content in a paragraph.</p>
-                        <small>And some small print.</small>
+
+                        <p className="mb-1">Deal rating - Deal text</p>
+                        <small>Release sale date</small>
                     </div>
                 </a>
             </div>
