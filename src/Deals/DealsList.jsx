235b9dd60a8d0af153d2f7e1eb38ecb5d0b38d91
acalvom <andreac.3108@gmail.com>
2021-11-26 17:35:22 +0100

    Add deals dynamically in the list & Styles

--- src/Deals/DealsList.jsx
+++ src/Deals/DealsList.jsx
@@ -1,49 +1,37 @@
-import React from 'react';
-
 const DealsList = (props) => {
     const deals = props.deals;
-    console.log(deals)
+
+    const msToDate = (s) => {
+        let date = new Date(s * 1000);
+        return date.toLocaleDateString("es");
+    }
 
     return (
         <div className="list-deals list-group">
-            <a href="#" className="list-group-item list-group-item-action">
-                <div className="img-deal d-none d-sm-block">
-                    <img src="https://via.placeholder.com/120" className="img-fluid" alt=""/>
-                </div>
-                <div className="text-deal">
-                    <div className="d-flex w-100 justify-content-between">
-                        <h5 className="mb-1">Game Title</h5>
-                        <small>3 days ago</small>
-                    </div>
-                    <div className="d-flex w-100 justify-content-end">
-                        <span>Sale Price</span>
-                        <span>Normal Price</span>
-                        <span>Discount</span>
-                    </div>
+            {
+                deals.length > 0 ?
+                    deals.map(item =>
+                        <a href="#" className="list-group-item list-group-item-action">
+                            <div className="img-deal d-none d-sm-block">
+                                <img src={item.thumb} className="img-fluid" alt={item.title}/>
+                            </div>
+                            <div className="text-deal">
+                                <div className="d-flex w-100 justify-content-between">
+                                    <h5 className="mb-1">{item.title}</h5>
+                                    <small>{msToDate(item.lastChange)}</small>
+                                </div>
+                                <div className="d-flex w-100 justify-content-end">
+                                    <span className="pvs">{parseFloat(item.salePrice).toFixed(2)}€</span>
+                                    <span className="pvp">{parseFloat(item.normalPrice).toFixed(2)}€</span>
+                                    <span>{parseFloat(item.savings).toFixed(0)}%</span>
+                                </div>
+                                <p className="mb-1">{item.dealRating}/10.0 - {item.steamRatingText}</p>
+                                <small>Game release date: {msToDate(item.releaseDate)}</small>
+                            </div>
+                        </a>
+                    ) : <h5>No deals to show</h5>
+            }
 
-                    <p className="mb-1">Deal rating - Deal text</p>
-                    <small>Release sale date</small>
-                </div>
-            </a>
-            <a href="#" className="list-group-item list-group-item-action">
-                <div className="img-deal d-none d-sm-block">
-                    <img src="https://via.placeholder.com/120" className="img-fluid" alt=""/>
-                </div>
-                <div className="text-deal">
-                    <div className="d-flex w-100 justify-content-between">
-                        <h5 className="mb-1">Game Title</h5>
-                        <small>3 days ago</small>
-                    </div>
-                    <div className="d-flex w-100 justify-content-end">
-                        <span>Sale Price</span>
-                        <span>Normal Price</span>
-                        <span>Discount</span>
-                    </div>
-
-                    <p className="mb-1">Deal rating - Deal text</p>
-                    <small>Release sale date</small>
-                </div>
-            </a>
         </div>
     );
 };
