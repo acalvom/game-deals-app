d6696cfba5f3d695cf1ef2d01c27a6b9ecc6cff7
acalvom <andreac.3108@gmail.com>
2021-11-29 15:47:04 +0100

    Get/Set pageNumber & Freeze button if limit reached

--- src/components/Deals/DealsHome.jsx
+++ src/components/Deals/DealsHome.jsx
@@ -5,28 +5,33 @@ import Pagination from "../Pagination";
 
 function DealsHome() {
 
-    const API_DEALS = 'https://www.cheapshark.com/api/1.0/deals';
     const [deals, setDeals] = useState([]);
+    const [pageNumber, setPageNumber] = useState(0);
+    // const [isFetching, setIsFetching] = useState(true);
+    const API_DEALS = `https://www.cheapshark.com/api/1.0/deals?pageNumber=${pageNumber}`;
+
     const fetchDeals = async (url) => {
         try {
             const res = await fetch(url);
             const data = await res.json();
+            // setIsFetching(true);
             setDeals(data);
             console.log(data);
         } catch (e) {
             console.log(e);
         } finally {
+            // setIsFetching(false);
             console.log('End of request');
         }
     }
     useEffect(() => {
         fetchDeals(API_DEALS).then();
-    }, [])
+    }, [pageNumber])
 
     return (
         <div className="container-fluid">
             <h2 className="text-secondary"> Deals Home Page</h2>
-            <Pagination/>
+            <Pagination pageNumber={pageNumber} setPageNumber={setPageNumber}/>
             <DealsList deals={deals}/>
         </div>
     );
--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Pagination = (props) => {
+    const setPageNumber = props.setPageNumber;
+    const pageNumber = props.pageNumber;
+    const [disable, setDisable] = useState(true);
+
+    useEffect(() => {
+        pageNumber === 0 ? setDisable(true) : setDisable(false)
+    }, [pageNumber, disable])
+
     const prevPage = () => {
-        console.log('prev')
+        setPageNumber(pageNumber - 1);
     }
 
     const nextPage = () => {
-        console.log('next')
+        setPageNumber(pageNumber + 1)
     }
 
     return (
         <div className="d-flex w-100 justify-content-between my-3">
-            <button type="button" className="btn btn-outline-primary" onClick={prevPage}>Previous</button>
+            <button type="button" className="btn btn-outline-primary" disabled={disable} onClick={prevPage}>Previous
+            </button>
             <button type="button" className="btn btn-outline-primary" onClick={nextPage}>Next</button>
         </div>
     )
