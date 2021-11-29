2791281121fed2cbd94fdd8104e16036e8fc4604
acalvom <andreac.3108@gmail.com>
2021-11-29 16:44:28 +0100

    Control fetching data

--- src/components/Deals/DealsHome.jsx
+++ src/components/Deals/DealsHome.jsx
@@ -7,32 +7,32 @@ function DealsHome() {
 
     const [deals, setDeals] = useState([]);
     const [pageNumber, setPageNumber] = useState(0);
-    // const [isFetching, setIsFetching] = useState(true);
+    const [isFetching, setIsFetching] = useState(true);
     const API_DEALS = `https://www.cheapshark.com/api/1.0/deals?pageNumber=${pageNumber}`;
 
     const fetchDeals = async (url) => {
         try {
+            setIsFetching(true);
             const res = await fetch(url);
             const data = await res.json();
-            // setIsFetching(true);
             setDeals(data);
-            console.log(data);
         } catch (e) {
             console.log(e);
         } finally {
-            // setIsFetching(false);
+            setIsFetching(false);
             console.log('End of request');
         }
     }
     useEffect(() => {
         fetchDeals(API_DEALS).then();
-    }, [pageNumber])
+    }, [API_DEALS])
 
     return (
         <div className="container-fluid">
             <h2 className="text-secondary"> Deals Home Page</h2>
-            <Pagination pageNumber={pageNumber} setPageNumber={setPageNumber}/>
-            <DealsList deals={deals}/>
+            {isFetching && <div><h2>Fetching data...</h2></div>}
+            {!isFetching && <Pagination pageNumber={pageNumber} setPageNumber={setPageNumber}/>}
+            {!isFetching && <DealsList deals={deals}/>}
         </div>
     );
 }
