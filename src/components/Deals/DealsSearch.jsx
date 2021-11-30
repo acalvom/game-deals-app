a05eb128024d5be0ca148ca291f607b5a09cff03
acalvom <andreac.3108@gmail.com>
2021-11-30 10:43:47 +0100

    Add Submit/Reset buttons on form & Logic

--- src/components/Deals/DealsSearch.jsx
+++ src/components/Deals/DealsSearch.jsx
@@ -1,14 +1,29 @@
-import {Fragment} from "react";
+import {useState} from "react";
 
 const DealsSearch = () => {
+    const [title, setTitle] = useState("");
+
+    const sendTitle = (evt) => {
+        evt.preventDefault()
+        console.log('titulo: ', title)
+    }
+
+    const clearInput = (evt) => {
+        evt.preventDefault();
+        setTitle("");
+    }
+
     return (
-        <Fragment>
-            <div className="my-3">
+        <form onSubmit={sendTitle} onReset={clearInput}>
+            <div className="d-flex w-100 my-3">
                 <label htmlFor="gameTitleInput" className="form-label" hidden>Game title</label>
                 <input type="text" className="form-control" id="gameTitleInput"
-                       placeholder="Type a title to search deals..."/>
+                       placeholder="Type a title to search deals..." value={title}
+                       onChange={(evt) => setTitle(evt.target.value)}/>
+                <button className="btn btn-secondary mx-1" type="submit"><span className="fa fa-search"/></button>
+                <button className="btn btn-secondary mx-1" type="reset"><span className="fa fa-undo-alt"/></button>
             </div>
-        </Fragment>
+        </form>
     );
 };
 
