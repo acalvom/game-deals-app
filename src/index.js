7f1b0437a804baa802e58e34998f9f44a60baacd
acalvom <andreac.3108@gmail.com>
2021-12-10 14:04:46 +0100

    Install react-router-dom & Connect app

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
     <React.StrictMode>
-        <App/>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
